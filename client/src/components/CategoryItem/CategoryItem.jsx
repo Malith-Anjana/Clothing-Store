208838bf3b3f0a57bdedfc4a7c714f7b5ab47895
Malith Anjana <malithanjana97@gmail.com>
2023-06-12 13:01:26 +0530

    Initial commit

---
+++ client/src/components/CategoryItem/CategoryItem.jsx
@@ -0,0 +1,16 @@
+import { Button, Container, Image, Info, Title } from "./styled.component";
+
+
+const CategoryItem = ({ item }) => {
+  return (
+    <Container>
+      <Image src={item.img} />
+      <Info>
+        <Title>{item.title}</Title>
+        <Button>SHOP NOW</Button>
+      </Info>
+    </Container>
+  );
+};
+
+export default CategoryItem;
