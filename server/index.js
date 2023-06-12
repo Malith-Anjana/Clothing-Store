2b44d3fc8c395e7a2df09de9d4428bcbf2f33388
Malith Anjana <malithanjana97@gmail.com>
2023-06-12 17:16:08 +0530

    server: create user authentication and authorization

--- server/index.js
+++ server/index.js
@@ -4,18 +4,25 @@ const dotenv = require('dotenv');
 const app = express();
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
+const productRoute = require('./routes/product');
 
 dotenv.config();
 
+const connectionParams = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
 mongoose
-  .connect(process.env.MONGO_URL )
+  .connect(process.env.MONGO_URL, connectionParams)
   .then(() => console.log("db connected"))
   .catch((err) => console.log(err));
 
 app.use(express.json());
 
-app.use('/api/user', userRoute);
+app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
+app.use('/api/products', productRoute);
 
 
 app.listen(process.env.PORT || 5000, () => {
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 router.post("/register", async (req, res) => {
   const newUser = new User({
@@ -15,6 +16,41 @@ router.post("/register", async (req, res) => {
   try {
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//LOGIN
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({username: req.body.username});
+
+    if(!user) return res.status(401).json({ message: "Wrong User Name" });
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+
+    const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+    const inputPassword = req.body.password;
+
+    if(originalPassword != inputPassword) return res.status(401).json({ message: "Wrong Password" });
+
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: "3d" }
+    );
+
+    const { password, ...others } = user._doc;
+    res.status(200).json({ ...others, accessToken });
   } catch (err) {
     res.status(500).json(err);
   }
